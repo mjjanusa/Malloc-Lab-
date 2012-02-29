b0dfcd63522254ba70b2808325d2db32eb45fbc9
nick_egboh <nick.egboh@gmail.com>
2012-02-29 13:49:31 -0600

    added some Macros to mm.c, may prove to be usful

--- malloclab-handout/mm.c
+++ malloclab-handout/mm.c
@@ -44,6 +44,36 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+///////////Saw this in text book & they are not system calls/////////////
+///////////So i think we can use them if needed//////////////////////////
+/* Basic constants and macros */
+ #define WSIZE 4 /* Word and header/footer size (bytes) */
+ #define DSIZE 8 /* Double word size (bytes) */
+ #define CHUNKSIZE (1<<12) /* Extend heap by this amount (bytes) */
+
+ #define MAX(x, y) ((x) > (y)? (x) : (y))
+
+ /* Pack a size and allocated bit into a word */
+ #define PACK(size, alloc) ((size) | (alloc))
+
+ /* Read and write a word at address p */
+ #define GET(p) (*(unsigned int *)(p))
+ #define PUT(p, val) (*(unsigned int *)(p) = (val))
+
+ /* Read the size and allocated fields from address p */
+ #define GET_SIZE(p) (GET(p) & ~0x7)
+ #define GET_ALLOC(p) (GET(p) & 0x1)
+
+ /* Given block ptr bp, compute address of its header and footer */
+ #define HDRP(bp) ((char *)(bp) - WSIZE)
+ #define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+ /* Given block ptr bp, compute address of next and previous blocks */
+ #define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+ #define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+ 
+ ////////////////////////////////////////////////////////////////////////
+ 
 /* 
  * mm_init - initialize the malloc package.
  */
